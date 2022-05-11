3d9e8595b196b36ac987287ce3e6f689f947aa9b
yuxinsong <yuxinsong@sofarsolar.com>
2022-05-11 11:25:37 +0800

    V1.0.0.16 20220511 数据库sys_oauth_user表中的Email字段及对应的实体更改

---
+++ backend/QMS.Database.Migrations/Migrations/20220511032252_V1.0.0.34.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QMS.Database.Migrations.Migrations.DefaultDb
+{
+    public partial class V10034 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "sys_oauth_user",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                comment: "邮箱",
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true,
+                oldComment: "邮箱")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "sys_oauth_user",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                comment: "邮箱",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true,
+                oldComment: "邮箱")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
