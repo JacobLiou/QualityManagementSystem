6a5b689b7a2942d6b302372d4fa1cb5c41c56890
wanghongyu <wanghongyu@sofarsolar.com>
2022-05-20 14:13:22 +0800

    V1.0.0.27 20220520 日志服务

---
+++ backend/QMS.Application.System/Service/Logservice/ILogDebugService.cs
@@ -0,0 +1,14 @@
+﻿using QMS.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QMS.Application.System.Service.Log
+{
+    public interface ILogDebugService
+    {
+        public Task AddLogDebug(SysLogDebug sysLogDebug);
+    }
+}
