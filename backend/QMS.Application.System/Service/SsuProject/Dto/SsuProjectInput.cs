2e0ac940490852906ea399cdcd2514b3ad92f871
yuxinsong <yuxinsong@sofarsolar.com>
2022-06-11 15:24:11 +0800

    V1.0.0.44 20220611 项目新增名称，项目负责人，排序必录，排序字段应大于等于0；问题新增产品非必录；

--- backend/QMS.Application.System/Service/SsuProject/Dto/SsuProjectInput.cs
+++ backend/QMS.Application.System/Service/SsuProject/Dto/SsuProjectInput.cs
@@ -24,12 +24,32 @@ namespace QMS.Application.System
         /// <summary>
         /// 排序
         /// </summary>
-        [DataValidation(ValidationTypes.Integer, ErrorMessage = "排序值应大于等于0")]
         public virtual int Sort { get; set; }
     }
 
-    public class AddSsuProjectInput : SsuProjectInput
+    /// <summary>
+    /// 项目新增参数
+    /// </summary>
+    public class AddSsuProjectInput
     {
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        [Required(ErrorMessage = "项目名称为必填项")]
+        public virtual string ProjectName { get; set; }
+
+        /// <summary>
+        /// 项目负责人
+        /// </summary>
+        [Required(ErrorMessage = "项目负责人为必填项")]
+        public virtual long DirectorId { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        [Required(ErrorMessage = "排序值为必填项")]
+        [DataValidation(SystemValidationTypes.NumberUnNegativeNumber, ErrorMessage = "排序值应大于等于0")]
+        public virtual int Sort { get; set; }
     }
 
     public class DeleteSsuProjectInput : BaseId
